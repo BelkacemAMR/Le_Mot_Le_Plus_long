0a322a2f74d6c99fd27e99ddc47545ce91a9d9ef
Belkacem <abel-and-co@hotmail.com>
2023-04-06 01:52:47 +0200

    Version Finale

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ def longest_word(text):
 # On test la fonction :
 
 # Les chaînes de caractères à tester
-tests = ["Hello World", "Le python est un langage de programmation", "Les chats sont mignons"]
+
+tests = ["Il fait beau aujourdhui ", "Le python est un langage de programmation", "il faut faire du sport souvent"]
 
 # Tester la fonction pour chaque chaîne de caractères
+
 for test in tests:
     resultat = longest_word(test)
     print("Pour la chaîne de caractères '{}', le mot le plus long est : '{}'".format(test, resultat))
